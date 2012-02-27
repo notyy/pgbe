8877edd9062de8e9a5e6204e598f469f7aaf36bf
notyy <notyycn@gmail.com>
2012-02-28 03:29:24 +0800

    test some sitemap

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -3,22 +3,25 @@ package bootstrap.liftweb
 import net.liftweb._
 import http.{LiftRules, NotFoundAsTemplate, ParsePath}
 import sitemap.{SiteMap, Menu, Loc}
+import Loc._
 import util.{ NamedPF }
-
-
+import http._
 
 class Boot {
   def boot {
-  
-    
-  
     // where to search snippet
     LiftRules.addToPackages("pgbe")
 
     // build sitemap
-    val entries = (List(Menu("Home") / "index") :::
-                  
-                  Nil)
+    val entries = List(
+        //TODO: use i18n
+        Menu("Home") / "index",
+        Menu("Article Detail") / "articleDetail" >> Hidden
+          >> Unless(
+              () => S.param("id").isEmpty,
+              () => RedirectResponse("index")),
+        Menu("Article List") / "articleList"
+        ) 
     
     LiftRules.uriNotFound.prepend(NamedPF("404handler"){
       case (req,failure) => NotFoundAsTemplate(
@@ -29,8 +32,5 @@ class Boot {
     
     // set character encoding
     LiftRules.early.append(_.setCharacterEncoding("UTF-8"))
-    
-    
-    
   }
 }
