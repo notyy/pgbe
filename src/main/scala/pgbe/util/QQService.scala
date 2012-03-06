264c40fcffd1d18a5ec332cee6d86151d57a1b2d
notyy <notyycn@gmail.com>
2012-03-06 23:54:53 +0800

    testing qq api

--- src/main/scala/pgbe/util/QQService.scala
+++ src/main/scala/pgbe/util/QQService.scala
@@ -5,11 +5,9 @@ import org.scribe.model.SignatureType
 import java.util.Scanner
 
 object QQService {
-  val apiKey = "100250924" //apikey=apiId和apiSecret=key，需要向腾讯申请
-  val apiSecret = "f4573000c321957d73b1126dd93992f6"
   val scanner = new Scanner(System.in)
 
-  def requestAuthUrl(callBackURI: String) = {
+  def requestAuthUrl(apiKey: String, apiSecret: String, callBackURI: String) = {
     val service: OAuthService = (new ServiceBuilder().provider(new QQApi())).
       apiKey(apiKey).apiSecret(apiSecret).callback(callBackURI).
       signatureType(SignatureType.QueryString).build()
