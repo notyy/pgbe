2ac58d7b4acc292adf341c235db7f958c4a15f66
notyy <notyycn@gmail.com>
2012-03-07 13:39:31 +0800

    test qq

--- src/main/scala/pgbe/snippet/QQLogin.scala
+++ src/main/scala/pgbe/snippet/QQLogin.scala
@@ -7,8 +7,9 @@ import pgbe.util.Config
 import pgbe.util.QQService
 import net.liftweb.common.Full
 import scala.xml.Text
+import net.liftweb.common.Logger
 
-class QQLogin {
+class QQLogin extends Logger {
   val pageUrl = S.uri
   val code = S.param("code")
   val state = S.param("state")
@@ -17,9 +18,8 @@ class QQLogin {
   val qqService = QQService.initService(callBack)
 
   def render = "a [href]" #> { in: NodeSeq =>
-
     (code, state) match {
-      case (Full(sCode), _) => S.notice("CODE:" + sCode); Text(QQService.requestAuthUrl(qqService))
+      case (Full(sCode), _) => info("CODE:" + sCode); S.notice("CODE:" + sCode); Text(QQService.requestAuthUrl(qqService))
       case _ => Text(QQService.requestAuthUrl(qqService))
     }
   }
